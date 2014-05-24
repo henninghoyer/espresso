64dce90cc173d4a9be2068fa74c97682aac1602b
Henning Hoyer <henninghoyer@gmail.com>
2014-05-24 22:01:07 +0200

    date input for computation of todays spend is now generated automatically.

--- routes/index.js
+++ routes/index.js
@@ -23,69 +23,40 @@ exports.reportview = function(db) {
 
   // This does work. Finally... :)
   return function(req, res) {
-    var map = function() {
-        emit( this.category, this.amount );
-    };
-    var reduce = function(key, values) {
+    var td = new Date();
+    var day = td.getDate();
+    var month = td.getMonth()+1;
+    var year = td.getFullYear();
+
+    if(day < 10) {
+      day = '0' + day;
+    }
+    if(month < 10) {
+      month = '0' + month;
+    }
+    var today = year + '-' + month + '-' + day;
+
+    var ary = db.collection('expenses').mapReduce(
+      function() {
+        emit(this.category, this.amount);
+      },
+      function(key, values) {
         return Array.sum(values);
-    };
- 
-    var expSum = db.collection('expenses').mapReduce(
-      map,
-      reduce,
-      { query:
+      },
+      {
+        query:
         {
-          added: {$gte: new ISODate("2014-04-22")}
+          added: {$gte: new Date(today)}
         },
         out: {inline: 1}
-      });
-    console.log(expSum);
-    // db.collection('expenses').find({added:{$gt: new Date("2014-04-20T00:00:0.000Z"), $lt: new Date("2014-04-22T00:00:00.000Z")}}).toArray(function (err, items) {
-      // if(err) {
-        // console.log(err);
-      // } else {
-        // console.log(items);
-        // res.render('report', {title: 'Espresso - Chill your Finances'});
-      // }
-    // });
-
-    // And this will return aggregated spend for a given day per category
-    // given the volume of data it really wouldn't make sense to pre-aggregate this on the server...
-    // db.expenses.mapReduce( function() {emit( this.category, this.amount ); }, function(key, values) { return Array.sum(values) },  { query: { added: {$gte: new ISODate("2014-04-22")}}, out: {inline: 1}})
-
-
-    //   db.expenses.mapReduce( 
-//   function(){emit(this._id, this.amount);}, 
-//   function(key, values){return Array.sum(values);},
-//   {
-//     query:{ timestamp: {$gte: ISODate("2014-04-21T00:00:01Z"), $lt: ISODate("2014-04-22T00:00:00Z")}},
-//     out:"expense_totals"
-//   }
-// );
-
-    // db.collection('expenses').mapReduce(
-    //   function() {
-    //     emit(this._id, this.amount);
-    //   },
-    //   function(key, values) {
-    //     return Array.sum(values);
-    //   },
-    //   {
-    //     // query: { 'timestamp': {$gt: startDate, $lt: endDate}},
-    //     query: { 'category': "groceries"},
-    //     out: {inline: 1}
-    //   },
-    //   function(err, res, stats) {
-    //     if(err) {
-    //       console.log(err);
-    //     } else {
-    //       console.log("Result: " + res);
-    //     }
-    //     // res.render
-    //   }
-    // );
-  };
-};
+      },
+      function(key, reducedVal) {
+        res.render('report', {'dailySums': reducedVal});
+        // console.log(reducedVal.length);
+      }
+    );//mapReduce
+  };//return function
+};//reportview
 
 exports.additem = function(db) {
   return function(req, res) {
